4fea8f6db9d10e06c677b0915d2858b53727e79d
N1mmu <nimanthran@gmail.com>
2022-08-02 22:00:47 +0530

    music.js needs work
    music.js not working properly as of now

--- script/music.js
+++ script/music.js
@@ -1,11 +1,22 @@
 'use strict'
 
+// Pre functions
+
+import songList from './json/songList.json' assert{type: 'json'};
+
+
+//get jsonData from songList.json
+
 
 // playlist
-const playlist_image_1 = document.getElementById("playlist_img_1")
+
+var songNo=1;
+
+const song_name_1 = document.getElementById("song_name_1")
+const playlist_img_1 = document.getElementById("playlist_img_1")
 const play = document.getElementById("song");
 const playlist1_playButton = document.getElementById("playButton_p1");
-const playlist1_pauseButton = document.getElementById("pauseButton_p1")
+const playlist1_pauseButton = document.getElementById("pauseButton_p1");
 
 var memPlaying_p1=0;
 
@@ -25,8 +36,23 @@ const clickSong = function(){
     
 }
 
-const endedSong = function(){
+ const endedSong = function(){
     memPlaying_p1=0;
     playlist1_playButton.style.display = "block";
     playlist1_pauseButton.style.display = "none";
 }
+
+ const songSelection = function(){
+    playlist_img_1.src = songList[songNo].icon;
+    song.src = songList[songNo].src;
+    song_name_1.innerHTML = songList[songNo].name;
+
+}
+
+// post load function
+
+document.addEventListener("DOMContentLoaded",function(){
+    songSelection();
+});
+
+
